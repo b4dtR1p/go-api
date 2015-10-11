230d967ae675509e03f3e9c60c08773d1849a7a4
b4dtR1p <pucci.alessandro84@gmail.com>
2015-10-11 17:46:40 +0200

    syntax fix

--- Item.go
+++ Item.go
@@ -3,13 +3,13 @@ package main
 import "time"
 
 type Item struct {
-	Id		  	int			`json:"id"`
-	Picture	  	string		`json:"picture"`
-    Name      	string    	`json:"name"`
-    Description string		`json:"description"`
-    Price		string		`json:"price"`
-    Completed 	bool      	`json:"completed"`
-    Date      	time.Time 	`json:"date"`
+	Id		  	int					`json:"_id"`
+	Picture	  	string				`json:"picture"`
+    Name      	string    			`json:"name"`
+    Description string				`json:"description"`
+    Price		string				`json:"price"`
+    Completed 	bool      			`json:"completed"`
+    Date      	time.Time 			`json:"date"`
 }
 
 type Items []Item
