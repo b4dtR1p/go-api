95bcb91e3c55da15f659838e4715289003f172c4
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-10-12 23:39:02 +0200

    go fmt

--- Handlers.go
+++ Handlers.go
@@ -1,120 +1,120 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "net/http"
-    "errors"
-    "github.com/boltdb/bolt"
-    "os"
-    "path"
-    "time"
-    "strconv"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/boltdb/bolt"
+	"net/http"
+	"os"
+	"path"
+	"strconv"
+	"time"
 )
 
 type Database struct {
-    // Bolt database.
-    db *bolt.DB
+	// Bolt database.
+	db *bolt.DB
 }
 
 // Errors
 var (
-    ErrBucketNotFound = errors.New("bucket not found")
+	ErrBucketNotFound = errors.New("bucket not found")
 )
 
 // Create and open a database.
 func NewDatabase(filename string) (*Database, error) {
-    // Open database
-    os.MkdirAll(path.Dir(filename), 0700)
-    db, err := bolt.Open(filename, 0600, &bolt.Options{Timeout: 5 * time.Second})
-    if err != nil {
-        return nil, err
-    }
-    d := &Database{db}
+	// Open database
+	os.MkdirAll(path.Dir(filename), 0700)
+	db, err := bolt.Open(filename, 0600, &bolt.Options{Timeout: 5 * time.Second})
+	if err != nil {
+		return nil, err
+	}
+	d := &Database{db}
 
-    // Return
-    return d, nil
+	// Return
+	return d, nil
 }
 
 // Close database
 func (db *Database) Close() {
-    db.db.Close()
-    db.db = nil
+	db.db.Close()
+	db.db = nil
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Welcome to my go api!")
+	fmt.Fprintln(w, "Welcome to my go api!")
 }
 
 func NewItemId() uint64 {
-    var id uint64
- 
-    err := db.db.Update(func(tx *bolt.Tx) error {
-        bucket, err := tx.CreateBucketIfNotExists([]byte("item"))
-        if err != nil {
-            return err
-        }
- 
-        id, err = bucket.NextSequence()
-            return err
-        })
- 
-        if err != nil {
-            panic(err)
-        }
- 
-        return id
+	var id uint64
+
+	err := db.db.Update(func(tx *bolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists([]byte("item"))
+		if err != nil {
+			return err
+		}
+
+		id, err = bucket.NextSequence()
+		return err
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return id
 }
- 
+
 func SaveItem(item *Item) error {
-    encoded, err := json.Marshal(item)
-    if err != nil {
-        return err
-    }
- 
-    return db.db.Update(func(tx *bolt.Tx) error {
-        bucket, err := tx.CreateBucketIfNotExists([]byte("item"))
-        if err != nil {
-            return err
-        }
- 
-        id := strconv.FormatUint(item.Id, 10)
-        err = bucket.Put([]byte(id), encoded)
-        if err != nil {
-            return err
-        }
- 
-        return nil
-    })
+	encoded, err := json.Marshal(item)
+	if err != nil {
+		return err
+	}
+
+	return db.db.Update(func(tx *bolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists([]byte("item"))
+		if err != nil {
+			return err
+		}
+
+		id := strconv.FormatUint(item.Id, 10)
+		err = bucket.Put([]byte(id), encoded)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
 }
- 
+
 func Items() []*Item {
-    var list []*Item
-    db.db.View(func(tx *bolt.Tx) error {
-        bucket := tx.Bucket([]byte("item"))
-        if bucket == nil {
-            return nil
-        }
- 
-        bucket.ForEach(func(k, v []byte) error {
-            item := &Item{}
-            json.Unmarshal(v, &item)
-            list = append(list, item)
-            return nil
-        })
-        return nil
-    })
-    return list
+	var list []*Item
+	db.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("item"))
+		if bucket == nil {
+			return nil
+		}
+
+		bucket.ForEach(func(k, v []byte) error {
+			item := &Item{}
+			json.Unmarshal(v, &item)
+			list = append(list, item)
+			return nil
+		})
+		return nil
+	})
+	return list
 }
- 
+
 func RemoveItem(item *Item) error {
-    return db.db.Update(func(tx *bolt.Tx) error {
-        bucket := tx.Bucket([]byte("item"))
-        if bucket == nil {
-            return nil
-        }
- 
-        id := strconv.FormatUint(item.Id, 10)
-        return bucket.Delete([]byte(id))
-    })
+	return db.db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("item"))
+		if bucket == nil {
+			return nil
+		}
+
+		id := strconv.FormatUint(item.Id, 10)
+		return bucket.Delete([]byte(id))
+	})
 }
--- Routes.go
+++ Routes.go
@@ -3,37 +3,37 @@ package main
 import "net/http"
 
 type Route struct {
-    Name        string
-    Method      string
-    Pattern     string
-    HandlerFunc http.HandlerFunc
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
 }
 
 type Routes []Route
 
 var routes = Routes{
-    Route{
-        "Index",
-        "GET",
-        "/api",
-        Index,
-    },
-    Route{
-        "ItemIndex",
-        "GET",
-        "/api/items",
-        ItemIndex,
-    },
-    Route{
-        "ItemCreate",
-        "POST",
-        "/api/items",
-        ItemCreate,
-    },
-    Route{
-        "ItemShow",
-        "GET",
-        "/api/items/{itemId}",
-        ItemShow,
-    },
+	Route{
+		"Index",
+		"GET",
+		"/api",
+		Index,
+	},
+	Route{
+		"ItemIndex",
+		"GET",
+		"/api/items",
+		ItemIndex,
+	},
+	Route{
+		"ItemCreate",
+		"POST",
+		"/api/items",
+		ItemCreate,
+	},
+	Route{
+		"ItemShow",
+		"GET",
+		"/api/items/{itemId}",
+		ItemShow,
+	},
 }
