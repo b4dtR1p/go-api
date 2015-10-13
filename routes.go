390b09d114ef5fce8294ce2a52f6343bf6bb494c
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-10-13 21:41:33 +0200

    Merge routing related files together

--- routes.go
+++ routes.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 type Route struct {
 	Name        string
@@ -36,4 +39,21 @@ var routes = Routes{
 		"/api/items/{itemId}",
 		ItemShow,
 	},
+}
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		var handler http.Handler
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+
+	}
+	return router
 }
